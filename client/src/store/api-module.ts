3ffe6b3690cfefec9c6723a31abb7780ea215678
Rob Anderson <robanderson@hey.com>
2022-06-21 16:09:17 +0100

    feat: add profile page

--- client/src/store/api-module.ts
+++ client/src/store/api-module.ts
@@ -11,12 +11,22 @@ import {
 import { env } from '@/plugins/env-plugin'
 import { SocketIoPlugin } from '@/plugins/socketio-plugin'
 import { StorageKey } from '@/lib/module'
+import { UserRegistration } from '@openlab/deconf-shared/dist/registration'
 
 // TODO: move non-ApiClient auth logic somewhere else
 // or move all api-key related logic here (e.g. setLocale)
 
+// TODO: move these fixes back to deconf
+class PdcApiClient extends DeconfApiClient {
+  getRegistration(): Promise<UserRegistration | null> {
+    return this.fetchJson<{ registration: UserRegistration }>(
+      this.getEndpoint('RegistrationRoutes.getRegistration')
+    ).then((d) => d?.registration ?? null)
+  }
+}
+
 export function apiModule(): ApiStoreModule {
-  const apiClient = new DeconfApiClient(env.SERVER_URL.href, {
+  const apiClient = new PdcApiClient(env.SERVER_URL.href, {
     endpointMap: {
       // TODO: migrate fix to deconf
       'AttendanceRoutes.getSessionAttendance': (sessionId) =>
--- client/src/views/ProfileView.vue
+++ client/src/views/ProfileView.vue
@@ -1,14 +1,94 @@
 <template>
-  <AppLayout>
-    <p>Profile</p>
-  </AppLayout>
+  <UtilLayout>
+    <ProfileView
+      v-if="user && profile"
+      api-module="api"
+      :fields="fields"
+      @logout="onLogout"
+      @unregister="onUnregister"
+    >
+      <!-- AddUserCalendar? -->
+      <div slot="preActions" />
+    </ProfileView>
+  </UtilLayout>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-import AppLayout from '@/components/PdcAppLayout.vue'
+import {
+  FullAuthToken,
+  mapApiState,
+  ProfileField,
+  ProfileView,
+} from '@openlab/deconf-ui-toolkit'
+
+import UtilLayout from '@/components/PdcUtilLayout.vue'
+import { StorageKey } from '@/lib/module'
+import languageData from '@/data/languages.json'
 
 export default Vue.extend({
-  components: { AppLayout },
+  components: { UtilLayout, ProfileView },
+  computed: {
+    ...mapApiState('api', ['user', 'profile']),
+    fields(): ProfileField[] {
+      if (!this.profile || !this.user) return []
+      return [
+        {
+          label: this.$t('deconf.profile.pdc.nameText'),
+          value: this.profile.name,
+        },
+        {
+          label: this.$t('deconf.profile.pdc.emailText'),
+          value: this.profile.email,
+        },
+        {
+          label: this.$t('deconf.profile.pdc.localeText'),
+          value: languageData[this.user.user_lang],
+        },
+        {
+          label: this.$t('deconf.profile.pdc.registeredText'),
+          value: this.profile.created.toLocaleString(),
+        },
+        {
+          label: this.$t('deconf.profile.pdc.whenText'),
+          value: this.iatToString((this.user as FullAuthToken).iat),
+        },
+      ]
+    },
+  },
+  mounted() {
+    this.fetchProfile()
+  },
+  methods: {
+    async fetchProfile() {
+      await this.$store.dispatch('api/fetchProfile')
+      if (!this.profile) this.onLogout()
+    },
+    onLogout() {
+      localStorage.removeItem(StorageKey.AuthToken)
+      setTimeout(() => window.location.reload(), 500)
+    },
+    onUnregister() {
+      // ...
+    },
+    iatToString(iat: number) {
+      const date = new Date(iat * 1000)
+      date.setMinutes(
+        date.getMinutes() + date.getTimezoneOffset(),
+        date.getSeconds(),
+        date.getMilliseconds()
+      )
+
+      return date.toLocaleString()
+    },
+  },
 })
 </script>
+
+<style lang="scss">
+// TODO: Hacky - hide the remove profile button
+.profileView > :nth-child(6),
+.profileView > :nth-child(7) {
+  display: none;
+}
+</style>
