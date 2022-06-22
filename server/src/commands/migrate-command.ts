e2a4a43e07624d56131fd4fc92cf47377f6143f5
Rob Anderson <robanderson@hey.com>
2022-06-22 15:03:33 +0100

    fix: stop concurrent migrations with a semaphore

--- server/src/commands/migrate-command.ts
+++ server/src/commands/migrate-command.ts
@@ -1,14 +1,16 @@
 import {
-  createEnv,
   DECONF_MIGRATIONS,
   MigrateRepository,
   MigrateService,
   PostgresService,
+  RedisService,
+  SemaphoreService,
 } from '@openlab/deconf-api-toolkit'
-import { createDebug } from '../lib/module.js'
+import { createDebug, createEnv } from '../lib/module.js'
 
 const debug = createDebug('cmd:migrate')
 
+const MIGRATE_SEMAPHORE = 'migrate_lock'
 const PDC_MIGRATIONS = [...DECONF_MIGRATIONS]
 
 export interface MigrateCommandOptions {
@@ -19,23 +21,39 @@ export async function migrateCommand(options: MigrateCommandOptions) {
   debug('start')
 
   const env = createEnv()
+  if (!env.REDIS_URL) throw new Error('REDIS_URL not set')
+
+  const store = new RedisService(env.REDIS_URL)
+  const semaphore = new SemaphoreService({ store })
 
   debug(
     'migrations %o',
     PDC_MIGRATIONS.map((m) => m.id)
   )
 
-  const postgres = new PostgresService({ env })
+  try {
+    const hasLock = await semaphore.aquire(MIGRATE_SEMAPHORE, 5000)
+    if (!hasLock) throw new Error('Failed to aquire lock')
+    debug('aquired lock %o', MIGRATE_SEMAPHORE)
 
-  debug('connecting to postgres')
-  await postgres.run(async (client) => {
-    const migrateRepo = new MigrateRepository(client)
-    const migrate = new MigrateService({ migrateRepo })
+    const postgres = new PostgresService({ env })
 
-    debug('running migrations')
-    await migrate.runMigrations(PDC_MIGRATIONS)
-  })
+    debug('connecting to postgres')
+    await postgres.run(async (client) => {
+      const migrateRepo = new MigrateRepository(client)
+      const migrate = new MigrateService({ migrateRepo })
 
-  debug('disconnecting')
-  await postgres.close()
+      debug('running migrations')
+      await migrate.runMigrations(PDC_MIGRATIONS)
+    })
+
+    debug('disconnecting')
+    await postgres.close()
+
+    debug('waiting 5s')
+    await new Promise((resolve) => setTimeout(resolve, 5000))
+  } finally {
+    await semaphore.release(MIGRATE_SEMAPHORE)
+    await store.close()
+  }
 }
